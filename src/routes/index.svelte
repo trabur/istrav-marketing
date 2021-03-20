9b8b578cd5f17fc831e97a8787cdac5c4c8a250b
istrav <travis.burandt@gmail.com>
2021-03-20 18:52:25 -0500

    ISTRAV

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,81 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+  import { onMount } from "svelte";
+
+	onMount(() => {
+		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+		particlesJS.load('particles-js', './particles.json', function() {
+			console.log('callback - particles.js config loaded');
+		});
+	})
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>The platform for web services - ISTRAV</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="masonry">
+	<div id="particles-js">
+		<div class="middle">
+			<div class="logo">
+				<span style="font-size: 3em; margin: 0 -0.1em;">IS</span>
+				<span style="vertical-align: top; font-size: 1.5em; line-height: 2.3em;">TRAV</span>
+			</div>
+			<h1 class="slogan">The platform for<br />web services.</h1>
+		</div>
+	</div>
+</div>
+<div class="masonry">
+	<div id="platform">
+		
+	</div>
+</div>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<style>
+	.masonry {
+		height: 100vh;
+		position: relative;
+	}
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	.middle {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+	}
+
+	.logo {
+		margin: -1em 0 0 0;
+		font-size: 4em;
+		font-weight: 800;
+		text-align: center;
+		font-style: italic;
+	}
+
+	.slogan {
+		font-weight: 600;
+		text-align: center;
+		margin-top: -1em;
+    font-size: 2.5em;
+	}
+
+	#platform {
+		background-color: #111;
+		overflow: hidden;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+	}
+
+	#particles-js {
+		background-color: #ee6e73;
+		overflow: hidden;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+	}
+</style>
