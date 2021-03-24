1a5c1bfed4bcaaa835fe1daed2aaca8ef8b4562e
istrav <travis.burandt@gmail.com>
2021-03-24 13:21:37 -0500

    marketing

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,1 +1,50 @@
-<slot></slot>
+
+{#if loading}
+<!-- do nothing -->
+{:else}
+  <slot></slot>
+{/if}
+
+<div id="code"></div>
+
+<script>
+import { onMount, onDestroy } from 'svelte';
+import axios from 'axios'
+
+let loading = true
+
+onMount(() => {
+  // fetch library
+  window.axios = axios
+
+  // appId domain state
+  if (window.location.host === 'localhost:3000') {
+    window.appdomainId = 'istrav.com'
+  } else {
+    window.appDomain = window.location.host
+  }
+
+  // headless.istrav.com scripts
+  var code = document.createElement('script');
+  code.onload = function (event) {
+    // headless scripts & istrav will attach to window
+    // console.log(window.scripts)
+    // console.log(window.istrav)
+
+    istrav.tenant.members.init({ host: 'https://api.istrav.com' })
+    istrav.tenant.apps.init({ host: 'https://api.istrav.com' })
+    istrav.app.menus.init({ host: 'https://api.istrav.com' })
+    istrav.app.pages.init({ host: 'https://api.istrav.com' })
+    istrav.store.collections.init({ host: 'https://api.istrav.com' })
+    istrav.store.categories.init({ host: 'https://api.istrav.com' })
+    istrav.store.products.init({ host: 'https://api.istrav.com' })
+    istrav.account.users.init({ host: 'https://api.istrav.com' })
+    istrav.account.carts.init({ host: 'https://api.istrav.com' })
+    istrav.account.orders.init({ host: 'https://api.istrav.com' })
+    loading = false
+  };
+  code.src = 'https://headless.istrav.com/scripts/index.js';
+  code.setAttribute('type', 'module')
+  document.getElementById('code').appendChild(code); 
+})
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,26 +5,95 @@
 	import Solutions from '../components/Solutions.svelte'
 	import Nav from '../components/Nav.svelte'
 
-	onMount(() => {
+  let esApp
+  let appId
+  let domainId
+  let state = 'production'
+  let uploads
+  let rawApp = {
+    name: '',
+    short: ''
+  }
+
+	onMount(async () => {
 		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 		particlesJS.load('particles-js', './particles.json', function() {
 			console.log('callback - particles.js config loaded');
 		});
+
+    domainId = window.location.host
+
+    // pick an app to show for local development
+    if (domainId.includes('localhost:3000')) {
+      domainId = 'istrav.com'
+    }
+    // set appId from domain 
+    if (domainId.includes('dimension.click')) {
+      // for subdomains such as http://istrav.dimension.click
+      let endpoint = domainId.split('.')[0]
+      let esEndpoint = await scripts.tenant.apps.getEndpoint(null, endpoint)
+      if (esEndpoint.payload.success === true) {
+        esApp = esEndpoint.payload.data
+        appId = esEndpoint.payload.data.id
+        uploads = esEndpoint.payload.data.uploads
+        rawApp = JSON.parse(esEndpoint.payload.data.raw)
+        domainId = esEndpoint.payload.data.domain // do this so images load
+      } else {
+        alert(esEndpoint.payload.reason)
+      }
+    } else {
+      // for custom domains such as https://istrav.com
+      domainId = domainId.split('.').slice(-2).join('.')
+      let esOne = await scripts.tenant.apps.getOne(null, domainId, state)
+      if (esOne.payload.success === true) {
+        esApp = esOne.payload.data
+        appId = esOne.payload.data.id
+        uploads = esOne.payload.data.uploads
+        rawApp = JSON.parse(esOne.payload.data.raw)
+      } else {
+        alert(esOne.payload.reason)
+      }
+    }
 	})
+
+	// AAGHC
+	let show = false
+	function showMagic () {
+		show = true
+	}
+	function hideMagic () {
+		show = false
+	}
 </script>
 
 <svelte:head>
-	<title>ISTRAV: Web solutions and custom software.</title>
+	{#if rawApp.name}
+		<title>{rawApp.name}: {rawApp.sloganLine1} {rawApp.sloganLine2}</title>
+	{/if}
 </svelte:head>
 
 <div class="masonry">
 	<div id="particles-js">
 		<div class="middle">
-			<div class="logo">
-				<span style="font-size: 3em; margin: 0 -0.1em;">IS</span>
-				<span style="vertical-align: top; font-size: 1.5em; line-height: 2.3em;">TRAV</span>
-			</div>
-			<h1 class="slogan">Web solutions<br />and custom software.</h1>
+			{#if domainId === 'istrav.com'}
+				<div class="logo">
+					<span style="font-style: italic; font-size: 3em; margin: 0 -0.1em;">IS</span>
+					<span style="font-style: italic; vertical-align: top; font-size: 1.5em; line-height: 2.3em;">TRAV</span>
+				</div>
+			{:else if domainId === 'aaghc.com'}
+				<div class="aaghc" on:mouseenter={showMagic} on:mouseleave={hideMagic}>
+					{#if show}
+						AAGHC
+					{:else}
+						<div class="first-a">A</div><div class="second-a">A</div><div class="third-g">G</div><div class="rotate-letter">H</div><div class="space-letter">C</div>
+					{/if}
+				</div>
+			{:else}
+				<div class="logo">
+					<span style="font-size: 1.5em; line-height: 2em;">{rawApp.name || ''}</span>
+				</div>
+			{/if}
+			<h1 class="slogan">{rawApp.sloganLine1 || ''}<br />{rawApp.sloganLine2 || ''}</h1>
 			<div class="expand">
 				<a href="/#" class="btn-floating btn-large waves-effect waves-light" on:click={() => animateScroll.scrollTo({element: '#platform'})}>
 					<i class="material-icons">expand_more</i>
@@ -35,7 +104,9 @@
 </div>
 <Nav />
 <div id="platform">
-	<Solutions />
+	{#if domainId === 'istrav.com'}
+		<Solutions />
+	{/if}
 </div>
 <div class="dotted">
 	<div style="min-height: 100vh;"></div>
@@ -61,7 +132,6 @@
 		font-size: 4em;
 		font-weight: 800;
 		text-align: center;
-		font-style: italic;
 		color: #333;
 	}
 
@@ -96,5 +166,39 @@
     background-color: #eee;
     background-position: 0 0, 50px 50px;
     background-size: 100px 100px;
+	}
+
+	.aaghc {
+		margin: 0 auto;
+		font-size: 5em;
+		font-weight: 800;
+		text-align: center;
+		font-family: sans-serif;
+		color: #333;
+		cursor: pointer;
+		display: table;
+	}
+
+	.aaghc .first-a {
+		float: left;
+	}
+
+	.aaghc .second-a {
+		margin-left: -0.2em;
+		float: left;
+	}
+
+	.aaghc .third-g {
+		margin-right: 0.1em;
+		float: left;
+	}
+
+	.aaghc .rotate-letter {
+		transform: rotate(-90deg);
+		float: left;
+	}
+
+	.aaghc .space-letter {
+		float: left;
 	}
 </style>
