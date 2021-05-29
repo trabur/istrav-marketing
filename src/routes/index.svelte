f7a3947c61b8659c7baaf77b169e08c29fae717f
istrav <travis.burandt@gmail.com>
2021-05-28 19:43:06 -0500

    config backend

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,23 @@
 <script>
   import { onMount } from 'svelte';
 
+	import { appData } from '../stores.js';
   import { istrav, scripts } from '../../farmerless/api'
+
   import Page from '../../farmerless/components/Page.svelte'
 
   import { getStores, navigating, page, session } from '$app/stores';
 
   // When this is true, show the component
   let load = false
+
   let slug
 
   let app
-  let domainId
-  let state = 'production'
+  appData.subscribe(value => {
+    app = value
+    console.log(app)
+  })
 
   $: { reMount($page.params.slug) }
   // $: { reMount($page.params.state) }
@@ -23,43 +28,9 @@
     slug = s
     // state = $page.params.state
   }
-
-	onMount(async () => {
-    domainId = window.location.host
-
-    // pick an app to show for local development
-    if (domainId.includes('localhost:8000')) {
-      domainId = 'istrav.com'
-    }
-    // set appId from domain 
-    if (domainId.includes('tyu67.com')) {
-      // for subdomains such as http://istrav.tyu67.com
-      let endpoint = domainId.split('.')[0]
-      if (endpoint === 'www') {
-        endpoint = 'tyu67'
-      }
-      let esEndpoint = await scripts.tenant.apps.getEndpoint(null, endpoint)
-      if (esEndpoint.payload.success === true) {
-        app = esEndpoint.payload.data
-      } else {
-        alert(esEndpoint.payload.reason)
-      }
-    } else {
-      // for custom domains such as https://istrav.com
-      domainId = domainId.split('.').slice(-2).join('.')
-      let esOne = await scripts.tenant.apps.getOne(null, domainId, state)
-      if (esOne.payload.success === true) {
-        app = esOne.payload.data
-      } else {
-        alert(esOne.payload.reason)
-      }
-    }
-		console.log('app', app)
-	})
-
 </script>
 
-{#if load && app}
+{#if load && app.id}
 	{#if app.marketing}
   	<Page {app} slug={app.marketing.slug} />
 	{:else}
